0e1a20703a3efdb0877b9767838dfb65f647c7fa
Dginozator <viz@dginozator.com>
2023-03-30 19:53:22 +0300

    v_0_0_6

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-cd /home/
+sudo add-apt-repository ppa:jonathonf/rustlang
+sudo apt-get update
+sudo apt install -y git rustc cargo nginx
+git clone https://github.com/Dginozator/warp_server_test
+# sudo nano /etc/nginx/sites-enabled/default
+sudo nginx -s reload
+cd ~/warp_server_test
+cargo run
